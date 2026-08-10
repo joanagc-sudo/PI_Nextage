332c70f592cb2be1f709b2f50e94fec9dbe135be
joanagc-sudo <joanagc.goncalves@gmail.com>
2026-08-10 07:47:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express()
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render("pagina")
+})
+
+app.get('/teste', (req, res) => {
+  res.render("teste")
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
